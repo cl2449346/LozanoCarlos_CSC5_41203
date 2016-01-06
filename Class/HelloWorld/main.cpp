6760356745dc217c6182b4a4472af3c3fb2931b7
Carlos Lozano <Carlos Lozano>
2016-01-06 01:26:03 -0800

    Signed-off-by: Carlos Lozano <clozano10@student.rcc.edu> Signed-off-by: Carlos Lozano <clozano10@student.rcc.edu>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,20 @@
+/* Author: Carlos Lozano
+ * Created on January 6, 2016, 1:19 AM
+ * Purpose: To try IDE, Hello World
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Hello world begins here
+int main(int argc, char** argv) {
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    int dum;
+    cin>>dum;
+
+    return 0;
+}
+
