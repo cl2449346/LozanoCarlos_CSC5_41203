22d535bf76be3ee368c35a8352a95ae9fa0a8f2e
Carlos Lozano <clozano10@student.rcc.edu>
2016-02-02 21:26:36 -0800

    Signed-off-by: Carlos Lozano <clozano10@student.rcc.edu>

---
+++ Class/Classwork/Feb.2/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Carlos Lozano
+ * Created on February 2, 2016, 6:20 PM
+ * Purpose: Marksort
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+//Global Constants
+//Function Prototypes
+void fillAry(int a[], int n);
+
+//Start Here
+int main(int argc, char** argv) {
+    
+    cout<<"Hello World"<<endl;
+    
+    return 0;
+}
+
+void fillAry(int a[],int n){
+    //
+}
